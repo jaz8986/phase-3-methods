42aba22b23bf22790cb06c9469d9282a93a2119a
Jaskiran Singh <jsingh8986@yahoo.com>
2023-01-29 19:52:09 -0800

    commit tree b7eb682ced15a8b27a5a4c9553205a5c08f1c8ed

--- methods.rb
+++ methods.rb
@@ -1,1 +1,22 @@
-# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(num1, num2)
+    return num1 + num2
+end
+
+def halve(integer)
+    if integer.class != Integer
+        return nil
+    end
+    return integer/2.to_i
+end
